9aae9aa71a54a68f5434b5709ae3c4dc8401b399
Koimtzoglou <dimitriskoimtzoglou@gmail.com>
2017-10-23 18:51:53 +0200

    Accuracy per class

---
+++ AccuracyPerClass.py
@@ -0,0 +1,47 @@
+import heapq
+def accuracy_per_class(model, x_data, y_data , level):
+    '''
+    A function that returns the accuracy per class based on the level of the probabilities.
+    
+    Parameters
+    ----------
+    model: A Keras Sequential model
+    x_data: array
+        The array of images
+    y_data: array
+        The hot encoded sequence of the labels
+    level: int
+        The level of the probabilities (Ask Mimis to tell you more about)
+     returns:
+         The accuracy per class
+    '''
+    if level>9:
+        raise ValueError('The level of should be up to the number of classes')
+    #A dictionary of our classes and their label
+    label = {0:'T-shirt/top', 1: 'Trouser', 2: 'Pullover', 3: 'Dress', 4: 'Coat', 
+             5: 'Sandal', 6: 'Shirt', 7:'Sneaker', 8: 'Bag', 9:'Ankle Boot'
+            }
+    #One hot to integer
+    one_hot_to_int = np.argmax(y_data, axis = 1)
+    #The number of the classes
+    unique_classes = set(one_hot_to_int)
+    #The prediction probabilities
+    predictions_prob = model.predict_proba(x_data)
+    #The indexes of each class in the x_data dataset
+    idxs_per_class = {key: list(np.where(one_hot_to_int == key)[0]) for key in unique_classes}
+    #An empty dictionary where we will fill our results
+    results = dict.fromkeys(unique_classes)
+    for every_class in idxs_per_class:
+        indices = idxs_per_class[every_class]
+        counter = 0
+        for index in indices:
+            y_true = one_hot_to_int[index]
+            y_pred = heapq.nlargest(level, range(len(predictions_prob[index])), key=predictions_prob[index].__getitem__)
+            if y_true in y_pred:
+                counter +=1 
+        accuracy = float(counter/len(indices))
+        print ('The class {0} has accuracy {1}'.format(label[every_class], accuracy))
+        results[every_class] = accuracy      
+        
+    return results    
+        
