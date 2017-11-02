b422f148c2cf3641b0f6b8d187a88f056631bb7c
Adam <zika.adam@gmail.com>
2017-11-02 11:06:00 +0100

    Base model hyperparam tuning.

--- classes/base_model.py
+++ classes/base_model.py
@@ -9,7 +9,8 @@ from sklearn.metrics import confusion_matrix
 from hyperopt import Trials, STATUS_OK, tpe, hp, fmin
 from hyperas.distributions import choice, uniform, conditional
 from hyperas import optim
-
+from keras.callbacks import LearningRateScheduler
+import classes
 
 class BaseModel(NNNetwork):
 
@@ -30,43 +31,93 @@ class BaseModel(NNNetwork):
         self.num_classes_to_predict = len(self.class_inds)
 
 
-    def train_model_for_tuning(self, params):
-        print('Tuning')
+    def __train_model_tuning(self, params):
+        '''Train a model during hyper params tuning.
+
+        @:param params: space of hyper params
+        @:return dict(loss, status, model)
+        '''
+
         model = Sequential()
-        model.add(Conv2D(32, (3, 3), activation='relu', input_shape=self.dh.input_shape))
-        model.add(Conv2D(64, (3, 3), activation='relu'))
+
+        model.add(Conv2D(params['filter_size1'], (params['kernel_size1'], params['kernel_size1']),
+                         activation=params['activation'], input_shape=self.dh.input_shape))
+        model.add(Conv2D(params['filter_size2'], (params['kernel_size2'], params['kernel_size2']),
+                         activation=params['activation']))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(params['dropout1']))
+
+        if params['num_layers']['layers'] == 'three':
+            model.add(Conv2D(params['num_layers']['filter_size3'],
+                             (params['num_layers']['kernel_size3'], params['num_layers']['kernel_size3']),
+                             activation=params['activation']))
+            model.add(Dropout(params['num_layers']['dropout3']))
+
         model.add(Flatten())
-        model.add(Dense(128, activation='relu'))
-        model.add(Dropout(0.5))
+        model.add(Dense(params['fc_size1'], activation=params['activation']))
+        model.add(Dropout(params['dropout2']))
         model.add(Dense(self.num_classes_to_predict, activation='softmax'))
 
-        model.compile(loss=keras.losses.categorical_crossentropy, optimizer='rmsprop', metrics=['accuracy'])
+
+        model.compile(loss=keras.losses.categorical_crossentropy, optimizer=params['optimizer'],
+                      metrics=['accuracy'])
+
         # converting 04567 labels into 01234
-        y_train_inds = [np.where(self.class_inds == y) for y in self.y_train]
+        y_train_inds = self.dh.get_base_index(self.y_train)
         # getting 01234 classes into one hot
         y_train_one_hot = keras.utils.to_categorical(y_train_inds, self.num_classes_to_predict)
-        model.fit(self.x_train, y_train_one_hot, batch_size=128, epochs=1)
 
-        y_test_inds = [np.where(self.class_inds == y) for y in self.y_test]
+        lrate = LearningRateScheduler(classes.step_decay)
+        callbacks_list = [lrate]
+        #earlyStopping = keras.callbacks.EarlyStopping(monitor='train_loss', min_delta, patience=3, verbose=0, mode='auto')
+
+        model.fit(self.x_train, y_train_one_hot, batch_size=128, epochs=params['nb_epochs'], callbacks = callbacks_list)
+
+        y_test_inds = self.dh.get_base_index(self.y_test)
         y_test_one_hot = keras.utils.to_categorical(y_test_inds, self.num_classes_to_predict)
-        score, acc = model.evaluate(self.x_test,y_test_one_hot, verbose=0)
+
+        score, acc = model.evaluate(self.x_test, y_test_one_hot, verbose=0)
+        print(params)
         print('Test accuracy:', acc)
         return {'loss': -acc, 'status': STATUS_OK, 'model': model}
 
-    def hyper_param_tuning(self):
-        print('Hyper param uning')
+    def tune_hyper_params(self, max_evals = 5):
+        '''Tune hyper parameters. Returns the best model'''
 
-        space = {'dropout1': hp.uniform('dropout1', .25, .75)}
+        # nice exmaple at https://github.com/fchollet/keras/issues/1591
+        space = {'num_layers': hp.choice('num_layers',
+                                     [{'layers': 'two', },
+                                      {'layers': 'three',
+                                       'filter_size3': hp.choice('filter_size3', [32, 64, 128]),
+                                       'kernel_size3': hp.choice('kernel_size3', [3, 4]),
+                                       'dropout3': hp.uniform('dropout3', .25, .75)}
+                                      ]),
+
+                 'filter_size1': hp.choice('filter_size1', [16, 32, 64]),
+                 'filter_size2': hp.choice('filter_size2', [16, 32, 64]),
+
+                 'kernel_size1': hp.choice('kernel_size1', [3, 4, 5]),
+                 'kernel_size2': hp.choice('kernel_size2', [3, 4]),
+
+                 'dropout1': hp.uniform('dropout1', .25, .75),
+                 'dropout2': hp.uniform('dropout2', .25, .75),
+
+                 'fc_size1': hp.choice('fc_size1', [64, 128, 256]),
+
+                 'nb_epochs': 5,
+                 'optimizer': 'adam',
+                 'activation': 'relu'
+                 }
 
         trials = Trials()
-        best = fmin(self.train_model_for_tuning, space, algo=tpe.suggest, max_evals=5, trials=trials)
+        best = fmin(self.__train_model_tuning, space, algo=tpe.suggest, max_evals=max_evals, trials=trials)
 
         return best
 
 
     def define_model_structure(self):
+        '''Defines arbitrary structure of a model.'''
+
         model = Sequential()
         model.add(Conv2D(32, (3, 3), activation='relu', input_shape=self.dh.input_shape))
         model.add(Conv2D(64, (3, 3), activation='relu'))
