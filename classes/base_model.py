b34df8c0029b073dae0fb03f6a06252a3d1d38db
Adam <zika.adam@gmail.com>
2017-11-02 13:01:53 +0100

    Hyperparams tuning of base model and transfer learning done.

--- classes/base_model.py
+++ classes/base_model.py
@@ -39,7 +39,6 @@ class BaseModel(NNNetwork):
         '''
 
         model = Sequential()
-
         model.add(Conv2D(params['filter_size1'], (params['kernel_size1'], params['kernel_size1']),
                          activation=params['activation'], input_shape=self.dh.input_shape))
         model.add(Conv2D(params['filter_size2'], (params['kernel_size2'], params['kernel_size2']),
@@ -58,7 +57,6 @@ class BaseModel(NNNetwork):
         model.add(Dropout(params['dropout2']))
         model.add(Dense(self.num_classes_to_predict, activation='softmax'))
 
-
         model.compile(loss=keras.losses.categorical_crossentropy, optimizer=params['optimizer'],
                       metrics=['accuracy'])
 
@@ -68,30 +66,30 @@ class BaseModel(NNNetwork):
         y_train_one_hot = keras.utils.to_categorical(y_train_inds, self.num_classes_to_predict)
 
         lrate = LearningRateScheduler(classes.step_decay)
-        callbacks_list = [lrate]
-        #earlyStopping = keras.callbacks.EarlyStopping(monitor='train_loss', min_delta, patience=3, verbose=0, mode='auto')
+        earlyStopping = keras.callbacks.EarlyStopping(monitor='acc', min_delta = 0.01, patience=3, verbose=0, mode='auto')
+        callbacks_list = [lrate, earlyStopping]
 
-        model.fit(self.x_train, y_train_one_hot, batch_size=128, epochs=params['nb_epochs'], callbacks = callbacks_list)
+        model.fit(self.x_train, y_train_one_hot, batch_size=128, epochs=params['nb_epochs'], callbacks = callbacks_list, verbose = 0)
 
         y_test_inds = self.dh.get_base_index(self.y_test)
         y_test_one_hot = keras.utils.to_categorical(y_test_inds, self.num_classes_to_predict)
 
         score, acc = model.evaluate(self.x_test, y_test_one_hot, verbose=0)
-        print(params)
-        print('Test accuracy:', acc)
+        #print(params)
+        print('Test accuracy of model {}/{} is {:.2%}:'.format(self.tuning_iter, self.max_evals,  acc))
+        self.tuning_iter+=1
         return {'loss': -acc, 'status': STATUS_OK, 'model': model}
 
-    def tune_hyper_params(self, max_evals = 5):
-        '''Tune hyper parameters. Returns the best model'''
+    def get_hyper_param_space(self):
+        '''Return the space of hyperparameters to tune the model.'''
 
-        # nice exmaple at https://github.com/fchollet/keras/issues/1591
         space = {'num_layers': hp.choice('num_layers',
-                                     [{'layers': 'two', },
-                                      {'layers': 'three',
-                                       'filter_size3': hp.choice('filter_size3', [32, 64, 128]),
-                                       'kernel_size3': hp.choice('kernel_size3', [3, 4]),
-                                       'dropout3': hp.uniform('dropout3', .25, .75)}
-                                      ]),
+                                         [{'layers': 'two', },
+                                          {'layers': 'three',
+                                           'filter_size3': hp.choice('filter_size3', [32, 64, 128]),
+                                           'kernel_size3': hp.choice('kernel_size3', [3, 4]),
+                                           'dropout3': hp.uniform('dropout3', .25, .75)}
+                                          ]),
 
                  'filter_size1': hp.choice('filter_size1', [16, 32, 64]),
                  'filter_size2': hp.choice('filter_size2', [16, 32, 64]),
@@ -104,15 +102,25 @@ class BaseModel(NNNetwork):
 
                  'fc_size1': hp.choice('fc_size1', [64, 128, 256]),
 
-                 'nb_epochs': 5,
+                 'nb_epochs': 40,
                  'optimizer': 'adam',
                  'activation': 'relu'
                  }
+        return space
+
+    def tune_hyper_params(self, max_evals):
+        '''Tune hyper parameters. Returns the best model'''
+
+        # nice exmaple at https://github.com/fchollet/keras/issues/1591
+        space = self.get_hyper_param_space()
 
         trials = Trials()
+        self.tuning_iter = 0
+        self.max_evals = max_evals
+        # tpe = Tree of Parzen Estimators
         best = fmin(self.__train_model_tuning, space, algo=tpe.suggest, max_evals=max_evals, trials=trials)
-
-        return best
+        best_model_acc = np.max([-result['loss'] for result in trials.results]) # it is interpreted as loss
+        return best, best_model_acc,  trials
 
 
     def define_model_structure(self):
@@ -132,12 +140,12 @@ class BaseModel(NNNetwork):
         model.add(Dense(self.num_classes_to_predict, activation='softmax'))
         self.model = model
 
-    def train_model(self, nb_epoch=5, model_name='base_model.h5', batch_size = 128, load_model=False):
+    def train_model(self, model_name, nb_epoch=5, batch_size = 128, load_model=False):
         NNNetwork.train_model(self, x = self.x_train, model = self.model, load_model = load_model, model_name = model_name)
         if not load_model:
             self.model.compile(loss=keras.losses.categorical_crossentropy, optimizer='rmsprop', metrics=['accuracy'])
             # converting 04567 labels into 01234
-            y_train_inds = [np.where(self.class_inds == y) for y in self.y_train]
+            y_train_inds = self.dh.get_base_index(self.y_train)
             # getting 01234 classes into one hot
             y_train_one_hot = keras.utils.to_categorical(y_train_inds,self.num_classes_to_predict)
             self.model.fit(self.x_train,  y_train_one_hot, batch_size=batch_size, epochs=nb_epoch)
--- classes/transfer_learning.py
+++ classes/transfer_learning.py
@@ -1,20 +1,11 @@
-import random
 import numpy as np
 import keras
 from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, Flatten, Lambda, Input
-from keras.initializers import RandomNormal
-from keras.optimizers import SGD, RMSprop, Adam
-from keras import backend as K
-from itertools import product
-from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from hyperopt import Trials, STATUS_OK, tpe, hp, fmin
 from keras.callbacks import LearningRateScheduler
-from sklearn.model_selection import train_test_split
-import math
-from classes.nn_network import NNNetwork
 from classes.base_model import BaseModel
-import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
+import classes
 
 
 class TransferLearning(BaseModel):
@@ -32,20 +23,70 @@ class TransferLearning(BaseModel):
         predictions = Dense(self.num_classes_to_predict, activation='softmax', name='softmax_new1')(x)  # new softmax layer
         self.model = Model(input=base_model.input, output=predictions)
 
-    def train_model(self, nb_epoch=5, model_name='transfer_learning.h5', batch_size = 128, load_model=False):
-        '''Trains transfer learning model.'''
+    def __train_model_tuning(self, params):
+        '''Train a model during hyper params tuning.
 
-        NNNetwork.train_model(self, x = self.x_train, model = self.model, load_model = load_model, model_name = model_name)
+        @:base_model: pretrained model
+        @:param params: space of hyper params
+        @:return dict(loss, status, model)
+        '''
 
-        if not load_model:
-            print("Training on {} isntances of data.".format(len(self.x_train)))
-            self.model.compile(loss=keras.losses.categorical_crossentropy, optimizer='rmsprop', metrics=['accuracy'])
-            # converting class inds (e.g. 04567) labels into e.g. 01234
-            y_train_inds = [np.where(self.class_inds == y) for y in self.y_train]
-            # getting 01234 classes into one hot
-            y_train_one_hot = keras.utils.to_categorical(y_train_inds, self.num_classes_to_predict)
-            self.model.fit(self.x_train, y_train_one_hot, batch_size=batch_size, epochs=nb_epoch, shuffle=True)
-            self.model.save(model_name)
+        # to make sure that the first layer wont be trained while the second not
+        self.base_model.layers[0].trainable = params['train_conv0'] and params['train_conv1']
+        self.base_model.layers[1].trainable = params['train_conv1']
+
+        # adding a dense layer behind flatten_1 layers (i.e., layer[-4])
+        x = Dense(params['fc_size0'], activation=params['activation'], name="dense_new0")(self.base_model.layers[-4].output)
+        x = Dropout(params['dropout0'], name='dropout_new0')(x)
+        predictions = Dense(self.num_classes_to_predict, activation='softmax', name='softmax_new0')(x)
+        model = Model(input=self.base_model.input, output=predictions)
+
+        model.compile(loss=keras.losses.categorical_crossentropy, optimizer=params['optimizer'],
+                      metrics=['accuracy'])
+
+        # converting 04567 labels into 01234
+        y_train_inds = self.dh.get_base_index(self.y_train)
+        # getting 01234 classes into one hot
+        y_train_one_hot = keras.utils.to_categorical(y_train_inds, self.num_classes_to_predict)
+
+        # setting callback
+        lrate = LearningRateScheduler(classes.step_decay)
+        earlyStopping = keras.callbacks.EarlyStopping(monitor='acc', min_delta = 0.01, patience=3, verbose=0, mode='auto')
+        callbacks_list = [lrate, earlyStopping]
+
+        model.fit(self.x_train, y_train_one_hot, batch_size=128, epochs=params['nb_epochs'], callbacks = callbacks_list, verbose = 0)
+
+        # model evaluation
+        y_test_inds = self.dh.get_base_index(self.y_test)
+        y_test_one_hot = keras.utils.to_categorical(y_test_inds, self.num_classes_to_predict)
+        score, acc = model.evaluate(self.x_test, y_test_one_hot, verbose=0)
+
+        print('Test accuracy of model {}/{} is {:.2%}:'.format(self.tuning_iter, self.max_evals,  acc))
+        self.tuning_iter+=1
+        return {'loss': -acc, 'status': STATUS_OK, 'model': model}
+
+    def get_hyper_param_space(self):
+        '''Return the space of hyperparameters to tune the model.'''
+
+        space = {'train_conv0': hp.choice('train_conv0', [True, False]),
+                 'train_conv1': hp.choice('train_conv1', [True, False]),
+
+                 'dropout0': hp.uniform('dropout0', .25, .75),
+                 'fc_size0': hp.choice('fc_size0', [64, 128, 256]),
+
+                 'nb_epochs': 40,
+                 'optimizer': 'adam',
+                 'activation': 'relu'
+                 }
+        return space
+
+    def tune_hyper_params(self, base_model, max_evals):
+        '''Tune hyper parameters. Returns the best model'''
+
+        # nice exmaple at https://github.com/fchollet/keras/issues/1591
+
+        self.base_model = base_model
+        return super(BaseModel, self).tune_hyper_params()
 
 
 
